40a09429735244764cf95b9d2d64ff618f1eac41
trpyobernolt <a106648@aup.edu>
2022-11-18 16:19:15 +0100

    Data Reader now reads all relevant data

--- data_reader.py
+++ data_reader.py
@@ -19,132 +19,377 @@ class RawSEDData:
     def __init__(self):
         
         #Get the raw data columns from each individual assignment
-       raw1 = set(['Signi70', 'Sp70',
-              'e_Sp70', 'Sp70/Sbg70', 'Sconv70', 'Stot70', 'e_Stot70', 'FWHMa70',
-              'FWHMb070', 'PA070', 'Signi160', 'Sp160', 'e_Sp160', 'Sp160/Sbg160',
-              'Sconv160', 'Stot160', 'e_Stot160', 'FWHMa160', 'FWHMb160', 'PA160',
-              'Signi250', 'Sp250', 'e_Sp250', 'Sp250/Sbg250', 'Sconv250', 'Stot250',
-              'e_Stot250', 'FWHMa250', 'FWHMb250', 'PA250', 'Signi350', 'Sp350',
-              'e_Sp350', 'Sp350/Sbg350', 'Sconv350', 'Stot350', 'e_Stot350',
-              'FWHMa350', 'FWHMb350', 'PA350', 'Signi500', 'Sp500', 'e_Sp500',
-              'Sp500/Sbg500', 'Stot500', 'e_Stot500', 'FWHMa500', 'FWHMb500', 'PA500',
-              'SigniNH2', 'NpH2', 'NpH2/NbgH2', 'NconvH2', 'NbgH2', 'FWHMaNH2',
-              'FWHMbNH2', 'PANH2', 'NSED', 'CSARflag'])
+        
+        #Cepheus
+       raw1 = ['Signi70','Sp70','Sp70/Sbg70','Sconv70','Stot70','FWHMa70',
+         'FWHMb070', 'PA070', 'Signi160','Sp160',
+         'Sp160/Sbg160',
+         'Sconv160',
+         'Stot160',
+         'FWHMa160',
+         'FWHMb160',
+         'PA160',
+         'Signi250',
+         'Sp250',
+         'Sp250/Sbg250',
+         'Sconv250',
+         'Stot250',
+         'FWHMa250',
+         'FWHMb250',
+         'PA250',
+         'Signi350',
+         'Sp350',
+         'Sp350/Sbg350',
+         'Sconv350',
+         'Stot350',
+         'FWHMa350',
+         'FWHMb350',
+         'PA350',
+         'Signi500',
+         'Sp500',
+         'Sp500/Sbg500',
+         'Stot500',
+         'FWHMa500',
+         'FWHMb500',
+         'PA500',
+         'SigniNH2',
+         'NpH2',
+         'NpH2/NbgH2',
+         'NconvH2',
+         'NbgH2',
+         'FWHMaNH2',
+         'FWHMbNH2',
+         'PANH2',
+         'NSED']
        
-       raw2 = set(['Signi070', 'Sp070', 'e_Sp070',
-              'Sp070/Sbg070', 'Sconv070', 'Stot070', 'e_Stot070', 'FWHMa070',
-              'FWHMb070', 'PA070', 'Signi160', 'Sp160', 'e_Sp160', 'Sp160/Sbg160',
-              'Sconv160', 'Stot160', 'e_Stot160', 'FWHMa160', 'FWHMb160', 'PA160',
-              'Signi250', 'Sp250', 'e_Sp250', 'Sp250/Sbg250', 'Sconv250', 'Stot250',
-              'e_Stot250', 'FWHMa250', 'FWHMb250', 'PA250', 'Signi350', 'Sp350',
-              'e_Sp350', 'Sp350/Sbg350', 'Sconv350', 'Stot350', 'e_Stot350',
-              'FWHMa350', 'FWHMb350', 'PA350', 'Signi500', 'Sp500', 'e_Sp500',
-              'Sp500/Sbg500', 'Stot500', 'e_Stot500', 'FWHMa500', 'FWHMb500', 'PA500',
-              'SigniNH2', 'NpH2', 'NpH2/Nbg', 'NconvH2', 'NbgH2', 'FWHMaNH2',
-              'FWHMbNH2', 'PANH2', 'NSED'])
-       
-       raw3 = set(['sig70', 'I70peak', 'e_I70peak',
-              'C70', 'I70conv', 'S70', 'e_S70', 'a70', 'b70', 'PA70', 'sig160',
-             'I160peak', 'e_I160peak', 'C160', 'I160conv', 'S160', 'e_S160', 'a160',
-              'b160', 'PA160', 'sig250', 'I250peak', 'e_I250peak', 'C250', 'I250conv',
-             'S250', 'e_S250', 'a250', 'b250', 'PA250', 'sig350', 'I350peak',
-            'e_I350peak', 'C350', 'I350conv', 'S350', 'e_S350', 'a350', 'b350',
-            'PA350', 'sig500', 'I500peak', 'e_I500peak', 'C500', 'S500', 'e_S500',
-              'a500', 'b500', 'PA500', 'sigNH2', 'NH2peak', 'CNH2', 'NH2conv',
-              'NH2bg', 'aNH2', 'bNH2', 'PANH2', 'Nsed'])
-       
-       raw4 = set(['Signi070', 'Sp070', 'e_Sp070',
-                      'Sp/Sbg070', 'Sconv070', 'Stot070', 'e_Stot070', 'FWHMa070', 'FWHMb070',
-                      'PA070', 'Signi160', 'Sp160', 'e_Sp160', 'Sp/Sbg160', 'Sconv160',
-                      'Stot160', 'e_Stot160', 'FWHMa160', 'FWHMb160', 'PA160', 'Signi250',
-                      'Sp250', 'e_Sp250', 'Sp/Sbg250', 'Sconv250', 'Stot250', 'e_Stot250',
-                      'FWHMa250', 'FWHMb250', 'PA250', 'Signi350', 'Sp350', 'e_Sp350',
-                      'Sp/Sbg350', 'Sconv350', 'Stot350', 'e_Stot350', 'FWHMa350', 'FWHMb350',
-                      'PA350', 'Signi500', 'Sp500', 'e_Sp500', 'Sp/Sbg500', 'Stot500',
-                      'e_Stot500', 'FWHMa500', 'FWHMb500', 'PA500', 'SigniNH2', 'NpH2',
-                      'NpH2/Nbg', 'NconvH2', 'NbgH2', 'FWHMaNH2', 'FWHMbNH2', 'PANH2', 'NSED',
-                      'CSARflag', 'CUTEXflag'])
-    
-       raw5 = set(['runNo', 'CoreName', 'RAJ2000', 'DEJ2000', 'Sig070', 'Sp070', 'e_Sp070',
-              'Sp070/Sbg070', 'Sconv070', 'Stot070', 'e_Stot070', 'FWHMa070',
-              'FWHMb070', 'PA070', 'Sig160', 'Sp160', 'e_Sp160', 'Sp160/Sbg160',
-              'Sconv160', 'Stot160', 'e_Stot160', 'FWHMa160', 'FWHMb160', 'PA160',
-              'Sig250', 'Sp250', 'e_Sp250', 'Sp250/Sbg250', 'Sconv250', 'Stot250',
-              'e_Stot250', 'FWHMa250', 'FWHMb250', 'PA250', 'Sig350', 'Sp350',
-              'e_Sp350', 'Sp350/Sbg350', 'Sconv350', 'Stot350', 'e_Stot350',
-              'FWHMa350', 'FWHMb350', 'PA350', 'Sig500', 'Sp500', 'e_Sp500',
-              'Sp500/Sbg500', 'Stot500', 'e_Stot500', 'FWHMa500', 'FWHMb500', 'PA500',
-              'SigNH2', 'NH2p', 'NH2p/NH2bg', 'NH2conv', 'NH2bg', 'FWHMaNH2',
-              'FWHMbNH2', 'PANH2', 'NSED', 'CuTExflag'])
-       
-       raw6 = set(['Signi070', 'Sp070', 'e_Sp070',
-              'Sp070/Sbg070', 'Sconv070', 'Stot070', 'e_Stot070', 'FWHMa070',
-              'FWHMb070', 'PA070', 'Signi160', 'Sp160', 'e_Sp160', 'Sp160/Sbg160',
-              'Sconv160', 'Stot160', 'e_Stot160', 'FWHMa160', 'FWHMb160', 'PA160',
-              'Signi250', 'Sp250', 'e_Sp250', 'Sp250/Sbg250', 'Sconv250', 'Stot250',
-              'e_Stot250', 'FWHMa250', 'FWHMb250', 'PA250', 'Signi350', 'Sp350',
-              'e_Sp350', 'Sp350/Sbg350', 'Sconv350', 'Stot350', 'e_Stot350',
-              'FWHMa350', 'FWHMb350', 'PA350', 'Signi500', 'Sp500', 'e_Sp500',
-              'Sp500/Sbg500', 'Stot500', 'e_Stot500', 'FWHMa500', 'FWHMb500', 'PA500',
-              'SigniNH2', 'NpH2', 'NpH2/Nbg', 'NconvH2', 'NbgH2', 'FWHMaNH2',
-              'FWHMbNH2', 'PANH2', 'NSED', 'CSARflag'])
-       
-       
-       raw7 = set(['Signi070', 'Sp070', 'e_Sp070',
-              'Sp070/Sbg070', 'Sconv070', 'Stot070', 'e_Stot070', 'FWHMa070',
-              'FWHMb070', 'PA070', 'Signi160', 'Sp160', 'e_Sp160', 'Sp160/Sbg160',
-              'Sconv160', 'Stot160', 'e_Stot160', 'FWHMa160', 'FWHMb160', 'PA160',
-              'Signi250', 'Sp250', 'e_Sp250', 'Sp250/Sbg250', 'Sconv250', 'Stot250',
-              'e_Stot250', 'FWHMa250', 'FWHMb250', 'PA250', 'Signi350', 'Sp350',
-              'e_Sp350', 'Sp350/Sbg350', 'Sconv350', 'Stot350', 'e_Stot350',
-              'FWHMa350', 'FWHMb350', 'PA350', 'Signi500', 'Sp500', 'e_Sp500',
-              'Sp500/Sbg500', 'Stot500', 'e_Stot500', 'FWHMa500', 'FWHMb500', 'PA500',
-              'SigniNH2', 'NpH2', 'NpH2/Nbg', 'NconvH2', 'NbgH2', 'FWHMaNH2',
-              'FWHMbNH2', 'PANH2', 'NSED'])
-       
-       #Take the intersection of the sets to only get data in common
-       common_elements = raw1.intersection(raw2, raw3, raw4, raw5, raw6, raw7)
-       
-       #Cepheus Table
        dataset = pd.read_csv('Data/cepheus_tablea1.tsv',delimiter=';',comment='#') 
-       self.X1 = dataset[common_elements]
-
+       self.X1 = dataset[raw1]
        self.Y1 = dataset['Coretype']
 
        #ophiuchus_tablea1
+       raw2 = ['Signi070',
+        'Sp070',
+        'Sp070/Sbg070',
+        'Sconv070',
+        'Stot070',
+        'FWHMa070',
+        'FWHMb070',
+        'PA070',
+        'Signi160',
+        'Sp160',
+        'Sp160/Sbg160',
+        'Sconv160',
+        'Stot160',
+        'FWHMa160',
+        'FWHMb160',
+        'PA160',
+        'Signi250',
+        'Sp250',
+        'Sp250/Sbg250',
+        'Sconv250',
+        'Stot250',
+        'FWHMa250',
+        'FWHMb250',
+        'PA250',
+        'Signi350',
+        'Sp350',
+        'Sp350/Sbg350',
+        'Sconv350',
+        'Stot350',
+        'FWHMa350',
+        'FWHMb350',
+        'PA350',
+        'Signi500',
+        'Sp500',
+        'Sp500/Sbg500',
+        'Stot500',
+        'FWHMa500',
+        'FWHMb500',
+        'PA500',
+        'SigniNH2',
+        'NpH2',
+        'NpH2/Nbg',
+        'NconvH2',
+        'NbgH2',
+        'FWHMaNH2',
+        'FWHMbNH2',
+        'PANH2',
+        'NSED']
        dataset = pd.read_csv('Data/ophiuchus_tablea1.tsv', delimiter=';',comment='#')
-       self.X2 = dataset[common_elements]
-
+       self.X2 = dataset[raw2]
        self.Y2 = dataset['Coretype']
-
+       
        #taurus_table1
+       raw3 = ['sig70',
+        'I70peak',
+        'C70',
+        'I70conv',
+        'S70',
+        'a70',
+        'b70',
+        'PA70',
+        'sig160',
+        'I160peak',
+        'C160',
+        'I160conv',
+        'S160',
+        'a160',
+        'b160',
+        'PA160',
+        'sig250',
+        'I250peak',
+        'C250',
+        'I250conv',
+        'S250',
+        'a250',
+        'b250',
+        'PA250',
+        'sig350',
+        'I350peak',
+        'C350',
+        'I350conv',
+        'S350',
+        'a350',
+        'b350',
+        'PA350',
+        'sig500',
+        'I500peak',
+        'C500',
+        'S500',
+        'a500',
+        'b500',
+        'PA500',
+        'sigNH2',
+        'NH2peak',
+        'CNH2',
+        'NH2conv',
+        'NH2bg',
+        'aNH2',
+        'bNH2',
+        'PANH2',
+        'Nsed']
        dataset = pd.read_csv('Data/taurus_table1.tsv', delimiter=';',comment='#')
-       self.X3 = dataset[common_elements]
+       self.X3 = dataset[raw3]
        self.Y3 = dataset['CType']
-
+       
+       """More work to be done here ^ """
+       
        #Corona Australia
+       raw4 = ['Signi070',
+        'Sp070',
+        'Sp/Sbg070',
+        'Sconv070',
+        'Stot070',
+        'FWHMa070',
+        'FWHMb070',
+        'PA070',
+        'Signi160',
+        'Sp160',
+        'Sp/Sbg160',
+        'Sconv160',
+        'Stot160',
+        'FWHMa160',
+        'FWHMb160',
+        'PA160',
+        'Signi250',
+        'Sp250',
+        'Sp/Sbg250',
+        'Sconv250',
+        'Stot250',
+        'FWHMa250',
+        'FWHMb250',
+        'PA250',
+        'Signi350',
+        'Sp350',
+        'Sp/Sbg350',
+        'Sconv350',
+        'Stot350',
+        'FWHMa350',
+        'FWHMb350',
+        'PA350',
+        'Signi500',
+        'Sp500',
+        'Sp/Sbg500',
+        'Stot500',
+        'FWHMa500',
+        'FWHMb500',
+        'PA500',
+        'SigniNH2',
+        'NpH2',
+        'NpH2/Nbg',
+        'NconvH2',
+        'NbgH2',
+        'FWHMaNH2',
+        'FWHMbNH2',
+        'PANH2',
+        'NSED']
+       
        dataset = pd.read_csv('Data/corona_australia_table1.tsv', delimiter=';',comment='#')
-       self.X4 = dataset[common_elements]
+       self.X4 = dataset[raw4]
        self.Y4 = dataset['Coretype']
-       #data-specific row engineering. Cannot be done post-merge
-                   
+    
        #lupus_tablea1
+       raw5 = ['Sig070',
+        'Sp070',
+        'Sp070/Sbg070',
+        'Sconv070',
+        'Stot070',
+        'FWHMa070',
+        'FWHMb070',
+        'PA070',
+        'Sig160',
+        'Sp160',
+        'Sp160/Sbg160',
+        'Sconv160',
+        'Stot160',
+        'FWHMa160',
+        'FWHMb160',
+        'PA160',
+        'Sig250',
+        'Sp250',
+        'Sp250/Sbg250',
+        'Sconv250',
+        'Stot250',
+        'FWHMa250',
+        'FWHMb250',
+        'PA250',
+        'Sig350',
+        'Sp350',
+        'Sp350/Sbg350',
+        'Sconv350',
+        'Stot350',
+        'FWHMa350',
+        'FWHMb350',
+        'PA350',
+        'Sig500',
+        'Sp500',
+        'Sp500/Sbg500',
+        'Stot500',
+        'FWHMa500',
+        'FWHMb500',
+        'PA500',
+        'SigNH2',
+        'NH2p',
+        'NH2p/NH2bg',
+        'NH2conv',
+        'NH2bg',
+        'FWHMaNH2',
+        'FWHMbNH2',
+        'PANH2',
+        'NSED']
+       
        dataset = pd.read_csv('Data/lupus_tablea11_14.tsv', delimiter=';',comment='#')
-       self.X5 = dataset[common_elements]
+       self.X5 = dataset[raw5]
        self.Y5 = dataset['Coretype']
-
+       
        #aquilia_table1
+       raw6 = ['Signi070',
+        'Sp070',
+        'Sp070/Sbg070',
+        'Sconv070',
+        'Stot070',
+        'FWHMa070',
+        'FWHMb070',
+        'PA070',
+        'Signi160',
+        'Sp160',
+        'Sp160/Sbg160',
+        'Sconv160',
+        'Stot160',
+        'FWHMa160',
+        'FWHMb160',
+        'PA160',
+        'Signi250',
+        'Sp250',
+        'Sp250/Sbg250',
+        'Sconv250',
+        'Stot250',
+        'FWHMa250',
+        'FWHMb250',
+        'PA250',
+        'Signi350',
+        'Sp350',
+        'Sp350/Sbg350',
+        'Sconv350',
+        'Stot350',
+        'FWHMa350',
+        'FWHMb350',
+        'PA350',
+        'Signi500',
+        'Sp500',
+        'Sp500/Sbg500',
+        'Stot500',
+        'FWHMa500',
+        'FWHMb500',
+        'PA500',
+        'SigniNH2',
+        'NpH2',
+        'NpH2/Nbg',
+        'NconvH2',
+        'NbgH2',
+        'FWHMaNH2',
+        'FWHMbNH2',
+        'PANH2',
+        'NSED']
        dataset = pd.read_csv('Data/aquila_table1.tsv', delimiter=';',comment='#')
-       self.X6 = dataset[common_elements]
+       self.X6 = dataset[raw6]
        self.Y6 = dataset['Coretype']
-
+       
        #orionb_table1
+       raw7 = ['Signi070',
+        'Sp070',
+        'Sp070/Sbg070',
+        'Sconv070',
+        'Stot070',
+        'FWHMa070',
+        'FWHMb070',
+        'PA070',
+        'Signi160',
+        'Sp160',
+        'Sp160/Sbg160',
+        'Sconv160',
+        'Stot160',
+        'FWHMa160',
+        'FWHMb160',
+        'PA160',
+        'Signi250',
+        'Sp250',
+        'Sp250/Sbg250',
+        'Sconv250',
+        'Stot250',
+        'FWHMa250',
+        'FWHMb250',
+        'PA250',
+        'Signi350',
+        'Sp350',
+        'Sp350/Sbg350',
+        'Sconv350',
+        'Stot350',
+        'FWHMa350',
+        'FWHMb350',
+        'PA350',
+        'Signi500',
+        'Sp500',
+        'Sp500/Sbg500',
+        'Stot500',
+        'FWHMa500',
+        'FWHMb500',
+        'PA500',
+        'SigniNH2',
+        'NpH2',
+        'NpH2/Nbg',
+        'NconvH2',
+        'NbgH2',
+        'FWHMaNH2',
+        'FWHMbNH2',
+        'PANH2',
+        'NSED']
+       
        dataset = pd.read_csv('Data/orionb_table1.tsv', delimiter=';',comment='#')
-       self.X7 = dataset[common_elements]
-
+       self.X7 = dataset[raw7]
        self.Y7 = dataset['Coretype']
        
-       print("X Columns that are used are: ", common_elements)
+       #print("X Columns that are used are: ", common_elements)
 
     def cleanData(self):    
        """Perform cleaning here"""
@@ -245,6 +490,11 @@ class RawSEDData:
             self.X7 = self.X7.drop(row)
             self.Y7 = self.Y7.drop(row)
        print("Orion Y labels: ", set(self.Y7))
+       
+       frames = [self.X1, self.X2, self.X3, self.X4, self.X5, 
+                      self.X6, self.X7]
+       for frame in frames:
+           frame.columns = self.X1.columns
 
        X = pd.concat([self.X1, self.X2, self.X3, self.X4, self.X5, 
                       self.X6, self.X7], ignore_index=True)
